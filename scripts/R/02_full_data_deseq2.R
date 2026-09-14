27f19e60e70ef7ba2b9a5dcec6ab99f9298ecee3
gsowah <149091901+gsowah@users.noreply.github.com>
2026-09-14 19:34:10 +0100

    Removed fitted object block

--- scripts/R/02_full_data_deseq2.R
+++ scripts/R/02_full_data_deseq2.R
@@ -79,9 +79,3 @@ write.csv(
   file.path(output_dir, "normalized_counts.csv"),
   row.names = FALSE
 )
-
-# fitted object for QC and downstream analyses
-saveRDS(
-  dds,
-  file.path(output_dir, "full_data_dds.rds")
-)
